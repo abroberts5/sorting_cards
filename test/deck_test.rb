7d5398382a3023039913053fae197713989fc622
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-08-21 22:17:17 -0600

    correction of class name

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,7 +4,7 @@ require './lib/deck'
 require 'minitest/autorun'
 require 'minitest/pride'
 
-class DeskTest < Minitest::Test
+class DeckTest < Minitest::Test
   def test_it_exist
     card = Card.new("10", "Hearts")
     guess = Guess.new("10 of Hearts", card)
