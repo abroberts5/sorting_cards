fcb3572303de27b7fe90a9393a21b17317ddb149
J Aaron Brooks Roberts <abroberts5@gmail.comabroberts5@gmail.comlsssh-keygen -t rsa -C abroberts5@gmail.com>
2018-10-01 19:43:57 -0600

    up to last method on iteration_2

--- lib/round.rb
+++ lib/round.rb
@@ -1,10 +1,12 @@
 class Round
   attr_reader   :deck,
-                :guesses
+                :guesses,
+                :number_correct
 
   def initialize(deck)
     @deck       = deck
     @guesses    = []
+    @number_correct = 0
   end
 
   def current_card
@@ -12,7 +14,16 @@ class Round
   end
 
   def record_guess(hash)
-    @guesses << hash
-    @guesses
+    guess_card = "#{hash[:value]} of #{hash[:suit]}"
+    new_guess = Guess.new(guess_card, current_card)
+    @guesses << new_guess
+    if new_guess.correct? == true
+      @number_correct += 1
+    end
+    new_guess
+  end
+
+  def percent_correct
+    @guesses.count.to_f / @number_correct.to_f
   end
 end
--- test/round_test.rb
+++ test/round_test.rb
@@ -36,8 +36,50 @@ class RoundTest < Minitest::Test
     card_2 = Card.new("4", "Clubs")
     deck = Deck.new([card_1, card_2])
     round = Round.new(deck)
+    assert_equal 0, round.guesses.count
     new_guess = round.record_guess({value: "3", suit: "Hearts"})
+    assert_equal 1, round.guesses.count
     assert Guess, new_guess.class
-    assert true, new_guess.correct?
+  end
+
+  def test_record_guesses_functionality
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    new_guess = round.record_guess({value: "3", suit: "Hearts"})
+    assert_equal true, new_guess.correct?
+  end
+
+  def test_number_correct_method
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    round.record_guess({value: "3", suit: "Hearts"})
+    assert_equal 1, round.guesses.count
+    assert_equal 1, round.number_correct
+  end
+
+  def test_it_can_have_multiple_guesses
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    round.record_guess({value: "3", suit: "Hearts"})
+    round.record_guess({value: "Jack", suit: "Diamonds"})
+    assert_equal 2, round.guesses.count
+    assert_equal "Incorrect.", round.guesses.last.feedback
+    assert_equal 1, round.number_correct
+  end
+
+  def test_percent_correct_method_works
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    round.record_guess({value: "3", suit: "Hearts"})
+    round.record_guess({value: "Jack", suit: "Diamonds"})
+    assert_equal 50.0, round.percent_correct
   end
 end
